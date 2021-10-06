2f05e4e07a87271b41f8ec745d1c798fc6de1e1b
ice987987 <mathias.frei@gmail.com>
2021-10-06 16:33:29 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'meteoblue adapter settings': {
+		'en': 'Adapter settings for meteoblue',
+		'de': 'Adaptereinstellungen für meteoblue',
+		'ru': 'Настройки адаптера для meteoblue',
+		'pt': 'Configurações do adaptador para meteoblue',
+		'nl': 'Adapterinstellingen voor meteoblue',
+		'fr': "Paramètres d'adaptateur pour meteoblue",
+		'it': "Impostazioni dell'adattatore per meteoblue",
+		'es': 'Ajustes del adaptador para meteoblue',
+		'pl': 'Ustawienia adaptera dla meteoblue',
+		'zh-cn': 'meteoblue的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
