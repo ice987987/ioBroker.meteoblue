21da0d27c4314703afd375b3caa929631e776a43
ice987987 <mathias.frei@gmail.com>
2021-10-15 16:41:53 +0200

    fix format

--- main.js
+++ main.js
@@ -52,25 +52,25 @@ class Meteoblue extends utils.Adapter {
 		this.log.debug('this.config.winddirection: ' + this.config.winddirection);
 		this.log.debug('this.config.precipitationamount: ' + this.config.precipitationamount);
 		this.log.debug('this.config.timeformat: ' + this.config.timeformat);
-	
+
 		//https://docs.meteoblue.com/en/weather-apis/packages-api/introduction#time-zone
-	
+
 		meteoblueAPIURL = 'http://my.meteoblue.com/packages/basic-day?';
 
 		//check apikey
 		if(typeof(this.config.apikey) === 'string') {
-			this.log.debug('APIKEY set. (' + this.config.apikey +')')
+			this.log.debug('APIKEY set. (' + this.config.apikey +')');
 			meteoblueAPIURL += 'apikey=' + this.config.apikey;
-			
+
 			//check and set latitute & longitude
 			if(typeof(this.config.latitude) === 'number' && !isNaN(this.config.latitude) && this.config.latitude >= -90 && this.config.latitude <= 90 &&
-			   typeof(this.config.longitude) === 'number' && !isNaN(this.config.longitude) && this.config.longitude >= -180 && this.config.longitude <= 180) {
+			typeof(this.config.longitude) === 'number' && !isNaN(this.config.longitude) && this.config.longitude >= -180 && this.config.longitude <= 180) {
 				this.log.info('latitude/longitude manually set');
 				meteoblueAPIURL += '&lat=' + this.config.latitude + '&lon=' + this.config.longitude;
 				await this.meteoblueAPIURL2ndPart();
 			} else {
-				this.log.info('latitude/longitude not manually set, get data from system')
-				
+				this.log.info('latitude/longitude not manually set, get data from system');
+
 				try {
 					const state = await this.getForeignObjectAsync('system.config');
 					this.config.latitude = state.common.latitude;
@@ -83,21 +83,21 @@ class Meteoblue extends utils.Adapter {
 				this.log.debug('system longitude: ' + this.config.longitude);
 
 				if(typeof(this.config.latitude) === 'number' && !isNaN(this.config.latitude) && this.config.latitude >= -90 && this.config.latitude <= 90 &&
-			       typeof(this.config.longitude) === 'number' && !isNaN(this.config.longitude) && this.config.longitude >= -180 && this.config.longitude <= 180) {
-					this.log.info('latitude/longitude set from system')
+				typeof(this.config.longitude) === 'number' && !isNaN(this.config.longitude) && this.config.longitude >= -180 && this.config.longitude <= 180) {
+					this.log.info('latitude/longitude set from system');
 					meteoblueAPIURL += '&lat=' + this.config.latitude + '&lon=' + this.config.longitude;
 					await this.meteoblueAPIURL2ndPart();
 				} else {
 					//shut down
-					this.log.error('latitude and/or longitude not set. Adapter will be terminated.')
-					this.setForeignState("system.adapter." + this.namespace + ".alive", false);
+					this.log.error('latitude and/or longitude not set. Adapter will be terminated.');
+					this.setForeignState('system.adapter.' + this.namespace + '.alive', false);
 				}
 			}
 		} else {
 			//shut down
-			this.log.error('APIKEY not set. Adapter will be terminated.')
-			this.setForeignState("system.adapter." + this.namespace + ".alive", false);
-		};
+			this.log.error('APIKEY not set. Adapter will be terminated.');
+			this.setForeignState('system.adapter.' + this.namespace + '.alive', false);
+		}
 	}
 
 	async meteoblueAPIURL2ndPart() {
@@ -128,12 +128,12 @@ class Meteoblue extends utils.Adapter {
 		}
 		meteoblueAPIURL += '&format=json';
 		this.log.info('meteoblueAPIURL: ' + meteoblueAPIURL);
-	
+
 		await this.createObjectsAPI();
 		await this.getMeteoblueData(meteoblueAPIURL);
 		await this.getMeteoblueDateIntervall(meteoblueAPIURL);
 	}
-	
+
 	async createObjectsAPI() {
 		this.log.info('create states...');
 
@@ -803,124 +803,122 @@ class Meteoblue extends utils.Adapter {
 	async getMeteoblueData(meteoblueAPIURL) {
 		//adapter.log.info('adapter: ' + adapter);
 		this.log.info('getMeteoblueData...');
-	
+
 		//https://www.npmjs.com/package/axios
 		axios({
 			method: 'get',
 			baseURL: meteoblueAPIURL,
-			//url: '/data.json',
-			//timeout: this.config.requestTimeout * 1000,
-			//responseType: 'json'
+
 		})
-		.then(async (response) => {
-	
-			this.setState('info.connection', true, true);
-	
-			const content = response.data;
-			this.log.debug('received data (' + response.status + '): ' + JSON.stringify(content));
-	
-			//metadata
-			this.setState('metadata.name', {val: content.metadata.name, ack: true});
-			//this.log.debug('metadata.name: ' + content.metadata.name);
-			this.setState('metadata.latitude', {val: content.metadata.latitude, ack: true});
-			//this.log.debug('metadata.latitude: ' + content.metadata.latitude);
-			this.setState('metadata.longitude', {val: content.metadata.longitude, ack: true});
-			//this.log.debug('metadata.longitude: ' + content.metadata.longitude);
-			this.setState('metadata.height', {val: content.metadata.height, ack: true});
-			this.setState('metadata.timezone_abbrevation', {val: content.metadata.timezone_abbrevation, ack: true});
-			this.setState('metadata.utc_timeoffset', {val: content.metadata.utc_timeoffset, ack: true});
-			if(typeof(content.metadata.modelrun_utc) === 'number') {
-				this.setState('metadata.modelrun_utc', {val: content.metadata.modelrun, ack: true});
-				this.setState('metadata.modelrun', {val: '', ack: true});
-			} else {
-				this.setState('metadata.modelrun_utc', {val: null, ack: true});
-				this.setState('metadata.modelrun', {val: content.metadata.modelrun, ack: true});
-			}
-			if(typeof(content.metadata.modelrun_utc) === 'number') {
-				this.setState('metadata.modelrun_updatetime_utc', {val: content.metadata.modelrun_updatetime_utc, ack: true});
-				this.setState('metadata.modelrun_updatetime', {val: '', ack: true});
-			} else {
-				this.setState('metadata.modelrun_updatetime_utc', {val: null, ack: true});
-				this.setState('metadata.modelrun_updatetime', {val: content.metadata.modelrun_updatetime_utc, ack: true});
-			}
-			
-			//units
-			this.setState('units.time', {val: content.units.time, ack: true});
-			this.setState('units.predictability', {val: content.units.predictability, ack: true});
-			this.setState('units.precipitation_probability', {val: content.units.precipitation_probability, ack: true});
-			this.setState('units.pressure', {val: content.units.pressure, ack: true});
-			this.setState('units.relativehumidity', {val: content.units.relativehumidity, ack: true});
-			this.setState('units.co', {val: content.units.co, ack: true});
-			this.setState('units.temperature', {val: content.units.temperature, ack: true});
-			this.setState('units.winddirection', {val: content.units.winddirection, ack: true});
-			this.setState('units.precipitation', {val: content.units.precipitation, ack: true});
-			this.setState('units.windspeed', {val: content.units.windspeed, ack: true});
-	
-			//data_day 0-6
-			for (let i = 0; i <= 6; i++) {
-				if(typeof(content.data_day.time[i]) === 'number') {
-					this.setState('data_day.' + i + '.time_UTC', {val: content.data_day.time[i], ack: true});
-					this.setState('data_day.' + i + '.time', {val: '', ack: true});
+			.then(async (response) => {
+
+				this.setState('info.connection', true, true);
+
+				const content = response.data;
+				this.log.debug('received data (' + response.status + '): ' + JSON.stringify(content));
+
+				//metadata
+				this.setState('metadata.name', {val: content.metadata.name, ack: true});
+				//this.log.debug('metadata.name: ' + content.metadata.name);
+				this.setState('metadata.latitude', {val: content.metadata.latitude, ack: true});
+				//this.log.debug('metadata.latitude: ' + content.metadata.latitude);
+				this.setState('metadata.longitude', {val: content.metadata.longitude, ack: true});
+				//this.log.debug('metadata.longitude: ' + content.metadata.longitude);
+				this.setState('metadata.height', {val: content.metadata.height, ack: true});
+				this.setState('metadata.timezone_abbrevation', {val: content.metadata.timezone_abbrevation, ack: true});
+				this.setState('metadata.utc_timeoffset', {val: content.metadata.utc_timeoffset, ack: true});
+				if(typeof(content.metadata.modelrun_utc) === 'number') {
+					this.setState('metadata.modelrun_utc', {val: content.metadata.modelrun, ack: true});
+					this.setState('metadata.modelrun', {val: '', ack: true});
 				} else {
-					this.setState('data_day.' + i + '.time_UTC', {val: null, ack: true});
-					this.setState('data_day.' + i + '.time', {val: content.data_day.time[i], ack: true});
+					this.setState('metadata.modelrun_utc', {val: null, ack: true});
+					this.setState('metadata.modelrun', {val: content.metadata.modelrun, ack: true});
 				}
-				this.setState('data_day.' + i + '.pictocode', {val: content.data_day.pictocode[i], ack: true});
-				this.setState('data_day.' + i + '.uvindex', {val: content.data_day.uvindex[i], ack: true});
-				this.setState('data_day.' + i + '.temperature_max', {val: content.data_day.temperature_max[i], ack: true});
-				this.setState('data_day.' + i + '.temperature_min', {val: content.data_day.temperature_min[i], ack: true});
-				this.setState('data_day.' + i + '.temperature_mean', {val: content.data_day.temperature_mean[i], ack: true});
-				this.setState('data_day.' + i + '.felttemperature_max', {val: content.data_day.felttemperature_max[i], ack: true});
-				this.setState('data_day.' + i + '.felttemperature_min', {val: content.data_day.felttemperature_min[i], ack: true});
-				if(typeof(content.data_day.winddirection[i]) === 'number') {
-					this.setState('data_day.' + i + '.winddirectionDeg', {val: content.data_day.winddirection[i], ack: true});
-					this.setState('data_day.' + i + '.winddirectionChar', {val: '', ack: true});
+				if(typeof(content.metadata.modelrun_utc) === 'number') {
+					this.setState('metadata.modelrun_updatetime_utc', {val: content.metadata.modelrun_updatetime_utc, ack: true});
+					this.setState('metadata.modelrun_updatetime', {val: '', ack: true});
 				} else {
-					this.setState('data_day.' + i + '.winddirectionDeg', {val: '', ack: true});
-					this.setState('data_day.' + i + '.winddirectionChar', {val: content.data_day.winddirection[i], ack: true});
+					this.setState('metadata.modelrun_updatetime_utc', {val: null, ack: true});
+					this.setState('metadata.modelrun_updatetime', {val: content.metadata.modelrun_updatetime_utc, ack: true});
 				}
-				this.setState('data_day.' + i + '.precipitation_probability', {val: content.data_day.precipitation_probability[i], ack: true});
-				this.setState('data_day.' + i + '.rainspot', {val: content.data_day.rainspot[i], ack: true});
-				this.setState('data_day.' + i + '.predictability_class', {val: content.data_day.predictability_class[i], ack: true});
-				this.setState('data_day.' + i + '.predictability', {val: content.data_day.predictability[i], ack: true});
-				this.setState('data_day.' + i + '.precipitation', {val: content.data_day.precipitation[i], ack: true});
-				this.setState('data_day.' + i + '.snowfraction', {val: content.data_day.snowfraction[i], ack: true});
-				this.setState('data_day.' + i + '.sealevelpressure_max', {val: content.data_day.sealevelpressure_max[i], ack: true});
-				this.setState('data_day.' + i + '.sealevelpressure_min', {val: content.data_day.sealevelpressure_min[i], ack: true});
-				this.setState('data_day.' + i + '.sealevelpressure_mean', {val: content.data_day.sealevelpressure_mean[i], ack: true});
-				this.setState('data_day.' + i + '.windspeed_max', {val: content.data_day.windspeed_max[i], ack: true});
-				this.setState('data_day.' + i + '.windspeed_mean', {val: content.data_day.windspeed_mean[i], ack: true});
-				this.setState('data_day.' + i + '.windspeed_min', {val: content.data_day.windspeed_min[i], ack: true});
-				this.setState('data_day.' + i + '.relativehumidity_max', {val: content.data_day.relativehumidity_max[i], ack: true});
-				this.setState('data_day.' + i + '.relativehumidity_min', {val: content.data_day.relativehumidity_min[i], ack: true});
-				this.setState('data_day.' + i + '.relativehumidity_mean', {val: content.data_day.relativehumidity_mean[i], ack: true});
-				this.setState('data_day.' + i + '.convective_precipitation', {val: content.data_day.convective_precipitation[i], ack: true});
-				this.setState('data_day.' + i + '.precipitation_hours', {val: content.data_day.precipitation_hours[i], ack: true});
-				this.setState('data_day.' + i + '.humiditygreater90_hours', {val: content.data_day.humiditygreater90_hours[i], ack: true});
-			}
-			this.log.info('all states written...');
-		})
-		.catch((error) => {
-			if (error.response) {
-				// The request was made and the server responded with a status code that falls out of the range of 2xx
-	
-				this.log.warn('received error ' + error.response.status +' with content: ' + JSON.stringify(error.response.data) + '(' + JSON.stringify(error.response.headers) +')');
-	
-			} else if (error.request) {
-				// The request was made but no response was received `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in node.js
-				this.log.info(error.message);
-	
-			} else {
-				// Something happened in setting up the request that triggered an Error
-				this.log.info(error.message);
-			}
-			this.log.info(error.config);
-		});
+
+				//units
+				this.setState('units.time', {val: content.units.time, ack: true});
+				this.setState('units.predictability', {val: content.units.predictability, ack: true});
+				this.setState('units.precipitation_probability', {val: content.units.precipitation_probability, ack: true});
+				this.setState('units.pressure', {val: content.units.pressure, ack: true});
+				this.setState('units.relativehumidity', {val: content.units.relativehumidity, ack: true});
+				this.setState('units.co', {val: content.units.co, ack: true});
+				this.setState('units.temperature', {val: content.units.temperature, ack: true});
+				this.setState('units.winddirection', {val: content.units.winddirection, ack: true});
+				this.setState('units.precipitation', {val: content.units.precipitation, ack: true});
+				this.setState('units.windspeed', {val: content.units.windspeed, ack: true});
+
+				//data_day 0-6
+				for (let i = 0; i <= 6; i++) {
+					if(typeof(content.data_day.time[i]) === 'number') {
+						this.setState('data_day.' + i + '.time_UTC', {val: content.data_day.time[i], ack: true});
+						this.setState('data_day.' + i + '.time', {val: '', ack: true});
+					} else {
+						this.setState('data_day.' + i + '.time_UTC', {val: null, ack: true});
+						this.setState('data_day.' + i + '.time', {val: content.data_day.time[i], ack: true});
+					}
+					this.setState('data_day.' + i + '.pictocode', {val: content.data_day.pictocode[i], ack: true});
+					this.setState('data_day.' + i + '.uvindex', {val: content.data_day.uvindex[i], ack: true});
+					this.setState('data_day.' + i + '.temperature_max', {val: content.data_day.temperature_max[i], ack: true});
+					this.setState('data_day.' + i + '.temperature_min', {val: content.data_day.temperature_min[i], ack: true});
+					this.setState('data_day.' + i + '.temperature_mean', {val: content.data_day.temperature_mean[i], ack: true});
+					this.setState('data_day.' + i + '.felttemperature_max', {val: content.data_day.felttemperature_max[i], ack: true});
+					this.setState('data_day.' + i + '.felttemperature_min', {val: content.data_day.felttemperature_min[i], ack: true});
+					if(typeof(content.data_day.winddirection[i]) === 'number') {
+						this.setState('data_day.' + i + '.winddirectionDeg', {val: content.data_day.winddirection[i], ack: true});
+						this.setState('data_day.' + i + '.winddirectionChar', {val: '', ack: true});
+					} else {
+						this.setState('data_day.' + i + '.winddirectionDeg', {val: '', ack: true});
+						this.setState('data_day.' + i + '.winddirectionChar', {val: content.data_day.winddirection[i], ack: true});
+					}
+					this.setState('data_day.' + i + '.precipitation_probability', {val: content.data_day.precipitation_probability[i], ack: true});
+					this.setState('data_day.' + i + '.rainspot', {val: content.data_day.rainspot[i], ack: true});
+					this.setState('data_day.' + i + '.predictability_class', {val: content.data_day.predictability_class[i], ack: true});
+					this.setState('data_day.' + i + '.predictability', {val: content.data_day.predictability[i], ack: true});
+					this.setState('data_day.' + i + '.precipitation', {val: content.data_day.precipitation[i], ack: true});
+					this.setState('data_day.' + i + '.snowfraction', {val: content.data_day.snowfraction[i], ack: true});
+					this.setState('data_day.' + i + '.sealevelpressure_max', {val: content.data_day.sealevelpressure_max[i], ack: true});
+					this.setState('data_day.' + i + '.sealevelpressure_min', {val: content.data_day.sealevelpressure_min[i], ack: true});
+					this.setState('data_day.' + i + '.sealevelpressure_mean', {val: content.data_day.sealevelpressure_mean[i], ack: true});
+					this.setState('data_day.' + i + '.windspeed_max', {val: content.data_day.windspeed_max[i], ack: true});
+					this.setState('data_day.' + i + '.windspeed_mean', {val: content.data_day.windspeed_mean[i], ack: true});
+					this.setState('data_day.' + i + '.windspeed_min', {val: content.data_day.windspeed_min[i], ack: true});
+					this.setState('data_day.' + i + '.relativehumidity_max', {val: content.data_day.relativehumidity_max[i], ack: true});
+					this.setState('data_day.' + i + '.relativehumidity_min', {val: content.data_day.relativehumidity_min[i], ack: true});
+					this.setState('data_day.' + i + '.relativehumidity_mean', {val: content.data_day.relativehumidity_mean[i], ack: true});
+					this.setState('data_day.' + i + '.convective_precipitation', {val: content.data_day.convective_precipitation[i], ack: true});
+					this.setState('data_day.' + i + '.precipitation_hours', {val: content.data_day.precipitation_hours[i], ack: true});
+					this.setState('data_day.' + i + '.humiditygreater90_hours', {val: content.data_day.humiditygreater90_hours[i], ack: true});
+				}
+				this.log.info('all states written...');
+			})
+			.catch((error) => {
+				if (error.response) {
+					// The request was made and the server responded with a status code that falls out of the range of 2xx
+
+					this.log.warn('received error ' + error.response.status +' with content: ' + JSON.stringify(error.response.data) + '(' + JSON.stringify(error.response.headers) +')');
+
+				} else if (error.request) {
+					// The request was made but no response was received `error.request` is an instance of XMLHttpRequest in the browser and an instance of http.ClientRequest in node.js
+					this.log.info(error.message);
+
+				} else {
+					// Something happened in setting up the request that triggered an Error
+					this.log.info(error.message);
+				}
+				this.log.info(error.config);
+			});
 	}
 
 	async getMeteoblueDateIntervall(meteoblueAPIURL) {
 		intervallGetMeteoblueData = setInterval(async () => {
-			await this.getMeteoblueData(meteoblueAPIURL)
+			await this.getMeteoblueData(meteoblueAPIURL);
 		}, 30*60000); //30*60000=1800000ms=30min
 	}
 
@@ -931,13 +929,8 @@ class Meteoblue extends utils.Adapter {
 	onUnload(callback) {
 		try {
 			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
-			callback();
 			clearInterval(intervallGetMeteoblueData);
+			callback();
 			this.log.info('cleaned everything up... (#1)');
 		} catch (e) {
 			callback();
